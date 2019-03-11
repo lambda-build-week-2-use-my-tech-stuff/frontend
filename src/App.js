a2f4dc5369deb4d22e9487434fbde426a6e5266d
VictorGoic0 <vgoico000@citymail.cuny.edu>
2019-03-11 16:28:33 -0400

    Moved everything up one level

--- src/App.js
+++ src/App.js
@@ -1,13 +1,10 @@
 import React, { Component } from 'react';
 import './App.css';
-<<<<<<< HEAD
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import Navigation from './components/navigation/Navigation';
 import PostPage from './components/catalog/PostPage';
-=======
 import PrimarySearchAppBar from './components/PrimarySearchAppBar'
 import Catalog from './components/Catalog';
->>>>>>> 29dcdf618fdf5fe10aa1aa5bacd261057eaf55e2
 
 class App extends Component {
   render() {
