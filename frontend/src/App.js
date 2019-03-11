ca3ab7e49eb1b194f5666e0d996e43164d4d0fe7
VictorGoic0 <vgoico000@citymail.cuny.edu>
2019-03-11 14:11:57 -0400

    AppBar imported from material-ui for Navigation. Default routes added to App. PostPage scaffolded. Default sizes set for certain elements. CSS reset added to index.css

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,26 +1,21 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import Navigation from './components/navigation/Navigation';
+import PostPage from './components/catalog/PostPage';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <Router>
+        <div className="App">
+          <PostPage />
+          <Route exact path="/" />
+          <Route path="/catalog" />
+          <Route path={`/catalog/postpage:id`} />
+          <Route path={`/profilepage/:id`} />
+        </div>
+      </Router>
     );
   }
 }
