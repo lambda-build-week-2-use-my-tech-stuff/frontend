0f6d055aee22e03ee76da87fdd7b8bc814eb242a
jkeslar21 <diamondroad91@gmail.com>
2019-03-11 16:15:58 -0700

    profilePage
    Co-authored-by: Rory <46462787+rmhennessey@users.noreply.github.com>

--- src/App.js
+++ src/App.js
@@ -5,13 +5,15 @@ import './App.css';
 // import PostPage from './components/catalog/PostPage';
 import PrimarySearchAppBar from './components/PrimarySearchAppBar'
 import Catalog from './components/Catalog';
+import ProfilePage from './components/ProfilePage';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <PrimarySearchAppBar/>
-        <Catalog />
+        {/* <PrimarySearchAppBar/>
+        <Catalog /> */}
+        <ProfilePage />
       </div>
     );
   }
