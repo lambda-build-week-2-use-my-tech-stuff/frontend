ddae9a28954d5fdb533bb1f46a3e451bd9caebed
VictorGoic0 <vgoico000@citymail.cuny.edu>
2019-03-12 11:52:45 -0400

    Merging

--- src/App.js
+++ src/App.js
@@ -1,19 +1,13 @@
 import React, { Component } from 'react';
 import './App.css';
-<<<<<<< HEAD
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import PostPage from './components/postpage/PostPage';
 import PrimarySearchAppBar from './components/PrimarySearchAppBar'
 import Catalog from './components/Catalog';
 import CatalogGrid from './components/CatalogGrid';
-=======
-// import { BrowserRouter as Router, Route } from 'react-router-dom';
-// import Navigation from './components/navigation/Navigation';
-// import PostPage from './components/catalog/PostPage';
 import PrimarySearchAppBar from './components/PrimarySearchAppBar'
 import Catalog from './components/Catalog';
 import ProfilePage from './components/ProfilePage';
->>>>>>> 094012ffcb18ded8a4ebe1458ddfa9eff271d829
 
 class App extends Component {
   render() {
