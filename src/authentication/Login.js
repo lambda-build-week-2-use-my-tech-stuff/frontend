9445a15e3c9a79793d0d7be9fe03ed7620619e50
jkeslar21 <diamondroad91@gmail.com>
2019-03-12 14:31:25 -0700

    auth
    Co-authored-by: Rory <46462787+rmhennessey@users.noreply.github.com>

---
+++ src/authentication/Login.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react'
+import './login.css';
+
+
+export default class Login extends Component {
+    constructor() {
+        super();
+        this.state = {
+            username: '',
+            password: '',
+        }
+    }
+
+    changeUserNameHandler = e => this.setState({ username: e.target.value });
+    changePasswordHandler = e => this.setState({ password: e.target.value });
+    submitDataHandler = () => {
+        const username = this.state.username;
+        const password = this.state.password;
+        localStorage.setItem('username', username);
+        localStorage.setItem('password', password);
+    }
+  render() {
+    return (
+      <div className='login'>
+        <form className='loginForm'>
+            <input className='loginInput' type='text' placeholder='Username' onChange={this.changeUserNameHandler} required />
+            <input className='loginInput' type='password' placeholder='Password' onChange={this.changePasswordHandler} required />
+            <button className='loginBtn' onClick={this.submitDataHandler}>Log In</button>
+            <br/>
+            <a href='#'>Sign Up</a>
+        </form>
+    </div>
+    )
+  }
+}
