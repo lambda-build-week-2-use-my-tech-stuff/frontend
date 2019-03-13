8241a32ec50fb3683ee547173eb9172e79683f3b
VictorGoic0 <vgoico000@citymail.cuny.edu>
2019-03-13 19:27:04 -0400

    window now reloads upon singing out and in

--- src/authentication/SignUp.js
+++ src/authentication/SignUp.js
@@ -23,7 +23,7 @@ class SignUp extends Component {
         .then(res => {
           console.log(res)
           localStorage.setItem('jwt', res.data.token)
-          this.props.history.push('/')
+          window.location.reload()
         })
         .catch(err => {
           console.log(err);
